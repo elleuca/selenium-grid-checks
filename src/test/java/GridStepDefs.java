c8b47e0d5f320954e83dbb1773fcb47af0cb4621
Luca Ferretti <elle.uca@gmail.com>
2013-11-01 21:48:05 +0100

    Start defining steps
    Using snippets provided by cucumber-jvm itself

---
+++ src/test/java/GridStepDefs.java
@@ -0,0 +1,41 @@
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+/**
+ *
+ */
+public class GridStepDefs {
+
+    @Given("^I perform a selenium test$")
+    public void I_perform_a_selenium_test() throws Throwable {
+        // Express the Regexp above with the code you wish you had
+        throw new PendingException();
+    }
+
+    @When("^I contact the hub$")
+    public void I_contact_the_hub() throws Throwable {
+        // Express the Regexp above with the code you wish you had
+        throw new PendingException();
+    }
+
+    @Then("^I should receive a reply from hub$")
+    public void I_should_receive_a_reply_from_hub() throws Throwable {
+        // Express the Regexp above with the code you wish you had
+        throw new PendingException();
+    }
+
+    @When("^I request a \"([^\"]*)\" driver$")
+    public void I_request_a_driver(String arg1) throws Throwable {
+        // Express the Regexp above with the code you wish you had
+        throw new PendingException();
+    }
+
+    @Then("^I should be able to use \"([^\"]*)\" driver$")
+    public void I_should_be_able_to_use_driver(String arg1) throws Throwable {
+        // Express the Regexp above with the code you wish you had
+        throw new PendingException();
+    }
+
+}
