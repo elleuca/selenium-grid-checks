43014f94117562b9791178940008d8f2153b3da5
Luca Ferretti <elle.uca@gmail.com>
2013-11-03 21:11:35 +0100

    Green lights for hub check steps
    Simplest check ever: open a connection to grid hub url
    and assert content type.

--- src/test/java/features/GridStepDefs.java
+++ src/test/java/features/GridStepDefs.java
@@ -5,27 +5,70 @@ import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 
+import java.io.IOException;
+import java.net.URL;
+import java.net.URLConnection;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
 /**
  *
  */
 public class GridStepDefs {
 
+
+    private GridHub gridHub;
+    private URL hubURL;
+    private URLConnection connection;
+
     @Given("^I perform a selenium test$")
     public void I_perform_a_selenium_test() throws Throwable {
-        // Express the Regexp above with the code you wish you had
-        throw new PendingException();
+        gridHub = new GridHub();
+        assertTrue(gridHub != null);
     }
 
     @When("^I contact the hub$")
     public void I_contact_the_hub() throws Throwable {
-        // Express the Regexp above with the code you wish you had
-        throw new PendingException();
+
+        hubURL = gridHub.getURL();
+
+        if (hubURL != null) {
+
+            try {
+                connection = hubURL.openConnection();
+            } catch (IOException e) {
+                fail();
+            }
+
+        } else {
+            fail();
+        }
+
+        if (connection  != null) {
+            try {
+                connection.connect();
+            } catch (IOException e) {
+                fail();
+            }
+        } else {
+            fail();
+        }
     }
 
     @Then("^I should receive a reply from hub$")
     public void I_should_receive_a_reply_from_hub() throws Throwable {
-        // Express the Regexp above with the code you wish you had
-        throw new PendingException();
+        String contentType = null;
+
+        try {
+            contentType = connection.getContentType();
+        } catch (Exception e) {
+            fail();
+        }
+
+        assertEquals("application/json;charset=UTF-8", contentType);
+
     }
 
     @When("^I request a \"([^\"]*)\" driver$")
